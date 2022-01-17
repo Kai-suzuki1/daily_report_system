492f6a543c14498afa811f3bf6e4614887b8852b
kai.suzuki <kaisuzuki1995@gmail.com>
2022-01-17 23:43:54 +0900

    persistence.xmlファイルの変更。createメソッド無事に動作
    - 接続タブのURL、ユーザー名、パスワード変更
    ブラウザ環境でアプリが動作するか確認するため。HerokuのURLだったためエラー発生

--- daily_report_system_dump.sql
+++ daily_report_system_dump.sql
@@ -16,6 +16,38 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `employee_id` int NOT NULL,
+  `report_id` int NOT NULL,
+  `content` longtext NOT NULL,
+  `comment_date` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employee_id` (`employee_id`),
+  KEY `report_id` (`report_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employees`
 --
 
@@ -116,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-11 18:19:16
+-- Dump completed on 2022-01-17 16:46:26
